a862a04eca7e3cfe7fac0e5ea539cb688884f08c
Maryna SAMOILENKO <msamoile@e3r10p4.42.fr>
2021-02-25 14:11:25 +0100

    c07-00

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msamoile <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 11:33:03 by msamoile          #+#    #+#             */
+/*   Updated: 2021/02/25 14:09:03 by msamoile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (dest == NULL)
+		return (NULL);
+	ft_strcpy(dest, src);
+		return (dest);
+}
+
+int	main()
+{
+	char src[] = "Hello";
+	printf("%s", ft_strdup(src));
+	//free(void );
+	return (0);
+}
